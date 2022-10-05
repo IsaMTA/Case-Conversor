6f95dd8d4515d4525bf8de802689c0a194836c62
Isacc <time.thief4561@gmail.com>
2022-10-05 08:34:37 -0500

    Case Conversor.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { snakeCase, capitalCase, pathCase, camelCase} from 'change-case';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'case-conversor';
+
+  caseForm = new FormGroup({
+    stringInput: new FormControl()
+  });
+
+  caseInput = '';
+
+  getString() {
+    if (this.caseForm.invalid) {
+      return;
+    }
+
+    this.caseInput = this.caseForm.get('stringInput')?.value;
+  }
+
+  clearString() {
+    this.caseInput = '';
+    this.caseForm.setValue({'stringInput': ''});
+  }
+
+  get toSnakeCase() {
+    return camelCase(this.caseInput);
+  }
+  get toPathCase() {
+    return pathCase(this.caseInput);
+  }
+  get toUpperCase() {
+    return capitalCase(this.caseInput);
+  }
 }
